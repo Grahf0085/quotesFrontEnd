1fff6ea6289b767fbbb22dbd4f3484a489f11d00
Tucker Hoog <tuckerhoog@tutanota.com>
2021-05-20 17:55:39 -0400

    quote utils

---
+++ src/app/Utils/quotes-api.js
@@ -0,0 +1,71 @@
+import request from 'superagent';
+
+export async function signUp(credentials) {
+  const response = await request
+    .post('/api/auth/signup')
+    // superagent considers 400 errors
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  // re-throw any bad request
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body;
+}
+
+export async function signIn(credentials) {
+  const response = await request
+    .post('/api/auth/signin')
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body;
+}
+
+export async function getQuotes(search) {
+  const response = await request
+    .get('/api/quotes')
+    .query({ filter: search })
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+// export async function getQuote(id) { // WE NEED BACKEND
+//   const response = await request
+//     .get(`/api/quotes/${id}`)
+//     .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+//   return response.body;
+// }
+
+export async function getMyQuotes() {
+  const response = await request
+    .get('/api/me/favorites')
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function addFavorite(favorite) {
+  const response = await request
+    .post('/api/favorites')
+    .send(favorite)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function deleteFavorite(id) {
+  const response = await request
+    .delete(`/api/favorites/${id}`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
